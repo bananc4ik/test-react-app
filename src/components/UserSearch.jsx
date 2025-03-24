580a24bacf45e8ebedc875abdcc0bf7f7ac6e608
bananc4ik <rkononcuk03@gmail.com>
2025-03-24 12:36:46 +0300

    MY_UPDATE

--- src/components/UserSearch.jsx
+++ src/components/UserSearch.jsx
@@ -9,6 +9,8 @@ function UserSearch() {
     const [users, setUsers] = useState([]);
     const [filterusers, setFilter] = useState([]);
 
+    const[data1, setData] = useState('');
+
     
 
     function change(event) {
@@ -32,25 +34,74 @@ function UserSearch() {
         
 
 
-    },[value])
+    },[value]);
 
-      
-      
+    function AllUsers() {
+        let users_str = "";
+        for (let index = 0; index < users.length; index++) {
+            users_str = users_str.concat(users[index].name);   
+            
+            
+        }
 
-    
-    
-    return(
+
         
-        <div>
-
-            <p>Value:{filterusers.map((user)=> user.name +", ")}</p>
-            <input onChange={change} id='input' type="text" />
-
-
-        </div>
         
+        let user_ls = document.getElementById("user_ls");
+        let ab =filterusers.map((user)=> user.name);
+        if (user_ls.textContent.includes(users_str)) {
+            console.log("A");
+            
+            
+        }else{
+            setFilter(users.filter((users) =>  users.name.includes(" ")));
+            setData(users.filter((users) =>  users.name.includes(" ")))
+        }
+
+        
+
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+
+       
+        
+        
+         
+    }
+
    
-    );
+
+    
+                    
+    return(
+            
+            <div >
+
+                <p id='user_ls'>{filterusers.map((user)=> user.name)}</p>
+                <input onChange={change} id='input' type="text" />
+                <button id='but' onClick={AllUsers}>Показать всех пользователей</button>
+
+
+            </div>
+            
+    
+        );
+      
+    
+
+    
+    
+    
+
+    
     
 }
 export default UserSearch;
